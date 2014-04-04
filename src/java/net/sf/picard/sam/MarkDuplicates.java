2992554821f798f75d028b598e7618a951fcf85b
Ian Sealy <ian.sealy@sanger.ac.uk>
2014-04-04 13:32:53 +0100

    Reads not ordered by tag, so some duplicates missed.

--- src/java/net/sf/picard/sam/MarkDuplicates.java
+++ src/java/net/sf/picard/sam/MarkDuplicates.java
@@ -753,6 +753,7 @@ public class MarkDuplicates extends AbstractDuplicateFindingAlgorithm {
             if (retval == 0) retval = lhs.orientation - rhs.orientation;
             if (retval == 0) retval = lhs.read2Sequence   - rhs.read2Sequence;
             if (retval == 0) retval = lhs.read2Coordinate - rhs.read2Coordinate;
+            if (retval == 0) retval = (int) (lhs.tag - rhs.tag);
             if (retval == 0) retval = (int) (lhs.read1IndexInFile - rhs.read1IndexInFile);
             if (retval == 0) retval = (int) (lhs.read2IndexInFile - rhs.read2IndexInFile);
 
